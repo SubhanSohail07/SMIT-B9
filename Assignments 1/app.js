d240a6a1ee47f44a3020d6e5cafb4516ddb5a6fc
SubhanSohail07 <subhansohailshah@gmail.com>
2023-01-26 22:06:01 +0500

    uncomment

---
+++ Assignments 1/app.js
@@ -0,0 +1,17 @@
+// *** Chapter 1
+// Question 1
+alert ("Hello Jee");
+
+
+// Question 2
+alert ("Error! Please Enter A Valid Password");
+
+// Question 3
+alert ("Welcome To JS Land...... \n Happy Coding")
+
+// Question 4
+alert ("Welcome to JS Land....");
+alert ("Happy Coding");
+
+// Question 5 
+alert ("Hello.... I can JS through web browser's consule");
