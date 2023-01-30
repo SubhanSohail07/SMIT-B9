13266adf3c570f7dfc7d30e480c5ed9ffc7f0fa8
SubhanSohail07 <subhansohailshah@gmail.com>
2023-01-31 03:07:50 +0500

    ASsignments of chapter 3,4, & 5.

---
+++ Chapter 2/app.js
@@ -0,0 +1,43 @@
+//chapter2
+//question1
+var username = "Subhan Sohail";
+document.write(username)
+
+//question2
+// var myname = "Subhan Sohail";
+// document.write(myname)
+
+//question3
+var myname = "Hello World";
+alert(myname)
+
+//question4
+var name = "Subhan Sohail";
+var age = "17 Years Old";
+var skill = "Content Creator";
+var course = "WMA";
+
+alert(name)
+alert(age)
+alert(skill)
+alert(course)
+
+//question5
+var dab = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(dab)
+
+// question6
+var email = "My Email Address Is msubhansohail03@gmail.com"
+alert(email);
+
+// question7
+var book = "A smarter way to learn JavaScript"
+alert(book)
+
+// question8
+var line8 = "</br> Yah! I can write HTML content through JavaScript"
+document.write(line8)
+
+// question9
+var fun = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(fun)
---
+++ Chapter 3/app.js
@@ -0,0 +1,16 @@
+// chapter3
+// question1
+var myage = "I Am 17 Years Old"
+alert(myage)
+
+// question2
+var visit = "You have visited this site 9 times"
+alert(visit)
+
+// question3
+var birthyear = "My birth year is 2005"
+document.write(birthyear)
+
+// question4
+var msg = "</br> Moosa Vayani ordered 5 shirt(s) on XYZ Clothing store"
+document.write(msg)
